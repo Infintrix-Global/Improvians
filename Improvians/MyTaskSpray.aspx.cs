d637dc689a691264bac87994dd8940e2d76dfeca
mehul Rana <mehul.rana@infintrixglobal.com>
2021-02-15 19:49:54 +0530

    Commit

--- Improvians/MyTaskSpray.aspx.cs
+++ Improvians/MyTaskSpray.aspx.cs
@@ -30,14 +30,15 @@ namespace Improvians
             NameValueCollection nv = new NameValueCollection();
 
             dt = objCommonControl.GetDataSet("SP_GetSprayEachTaskCount", nv);
-            lnkFertilization.Text = dt.Tables[0].Rows.Count.ToString();
 
 
-            //lnkGerm.Text = dt.Tables[1].Rows.Count.ToString();
-            //lnkFer.Text = dt.Tables[2].Rows.Count.ToString();
-            //lnkIrr.Text = dt.Tables[3].Rows.Count.ToString();
-            //lnkpr.Text = dt.Tables[4].Rows.Count.ToString();
-            //lnkMove.Text = dt.Tables[5].Rows.Count.ToString();
+            lnkPutAway.Text = dt.Tables[0].Rows.Count.ToString();
+
+            lnkGerm.Text = dt.Tables[1].Rows.Count.ToString();
+            lnkFertilization.Text = dt.Tables[2].Rows.Count.ToString();
+
+            lnkIrr.Text = dt.Tables[3].Rows.Count.ToString();
+            lnkpr.Text = dt.Tables[4].Rows.Count.ToString();
         }
     }
 }
