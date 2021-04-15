895a348a56901f4086ad7ad4690ae143c5f15f47
evo1 <evo1@MPH-GEM-02>
2021-04-15 07:58:02 -0700

    firebase setup

---
+++ Improvians/firebase-messaging-sw.js
@@ -0,0 +1,49 @@
+importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    apiKey: "AIzaSyDqjI1uzyT7VG2T39BLPALZDkcN3vJDLX8",
+    authDomain: "growerstrans-1172b.firebaseapp.com",
+    projectId: "growerstrans-1172b",
+    storageBucket: "growerstrans-1172b.appspot.com",
+    messagingSenderId: "342218804881",
+    appId: "1:342218804881:web:5ceb489d5eb65407aaf930",
+    measurementId: "G-1FYPTX710Q"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title1";
+    const notificationOptions = {
+        body: "Background Message body1.",
+        icon: "/itwonders-web-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+
+});
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
