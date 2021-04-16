bc311ead40c35af8a26d8d551d7f1240244edcc2
evo1 <evo1@MPH-GEM>
2021-04-16 00:52:53 -0700

    Commit

--- Improvians/CropReportRequestForm.aspx.cs
+++ Improvians/CropReportRequestForm.aspx.cs
@@ -109,25 +109,25 @@ namespace Evo
         }
         private void highlight()
         {
-            var i = gvPlantReady.Rows.Count;
-            bool check = false;
-            foreach (GridViewRow row in gvPlantReady.Rows)
-            {
-                var checkJob = (row.FindControl("lbljobID") as Label).Text;
-                var checklocation = (row.FindControl("lblBenchLoc") as Label).Text;
-                i--;
-                if (checkJob == JobCode && checklocation == benchLoc)
-                {
-                    row.CssClass = "highlighted";
-                    check = true;
-                }
-                if (i == 0 && !check)
-                {
-                    gvPlantReady.PageIndex++;
-                    gvPlantReady.DataBind();
-                    highlight();
-                }
-            }
+            //var i = gvPlantReady.Rows.Count;
+            //bool check = false;
+            //foreach (GridViewRow row in gvPlantReady.Rows)
+            //{
+            //    var checkJob = (row.FindControl("lbljobID") as Label).Text;
+            //    var checklocation = (row.FindControl("lblBenchLoc") as Label).Text;
+            //    i--;
+            //    if (checkJob == JobCode && checklocation == benchLoc)
+            //    {
+            //        row.CssClass = "highlighted";
+            //        check = true;
+            //    }
+            //    if (i == 0 && !check)
+            //    {
+            //        gvPlantReady.PageIndex++;
+            //        gvPlantReady.DataBind();
+            //        highlight();
+            //    }
+            //}
         }
         public void BindSupervisorList()
         {
@@ -235,151 +235,21 @@ namespace Evo
         {
             if (e.CommandName == "Select")
             {
-                userinput.Visible = true;
-                int rowIndex = Convert.ToInt32(e.CommandArgument);
-                HiddenFieldDid.Value = gvPlantReady.DataKeys[rowIndex].Values[1].ToString();
-                HiddenFieldJid.Value = gvPlantReady.DataKeys[rowIndex].Values[2].ToString();
-
-
-                DataTable dt = new DataTable();
-                NameValueCollection nv = new NameValueCollection();
-                nv.Add("@CropId", HiddenFieldDid.Value);
-
-                dt = objCommon.GetDataTable("SP_GeCropHealthReportRequestView", nv);
-
-                if (dt != null & dt.Rows.Count > 0)
-                {
-                    txtCommentsDump.Text = dt.Rows[0]["Comments"].ToString();
-                    txtQuantityofTray.Text = dt.Rows[0]["QuantityOfTray"].ToString();
-                    txtCropDate.Text = Convert.ToDateTime(dt.Rows[0]["CropHealthReportDate"]).ToString("yyyy-MM-dd");
-                }
-                //ddlSupervisor.Focus();
-
-
-            }
-
-            if (e.CommandName == "StartDump")
-            {
                 string ChId = "";
                 int rowIndex = Convert.ToInt32(e.CommandArgument);
                 string Did = gvPlantReady.DataKeys[rowIndex].Values[1].ToString();
                 string BatchLoc = gvPlantReady.DataKeys[rowIndex].Values[4].ToString();
                 string JobCode = gvPlantReady.DataKeys[rowIndex].Values[3].ToString();
-                //  ChId = gvPlantReady.DataKeys[rowIndex].Values[1].ToString();
-
-                if (ChId == "")
-                {
-                    ChId = "0";
-                }
-                else
-                {
-                    ChId = ChId;
-                }
+                Response.Redirect(String.Format("~/CropHealthReport.aspx?BatchLoc={0}&JobCode={1}}&CropAT={1}", BatchLoc, JobCode, Did));
 
 
-                NameValueCollection nv = new NameValueCollection();
-                nv.Add("@OperatorID", Session["LoginID"].ToString());
-                nv.Add("@Comments", "");
-                nv.Add("@CropId", Did);
-                nv.Add("@LoginID", Session["LoginID"].ToString());
-                nv.Add("@QuantityOfTray", "");
-                nv.Add("@CropDate", "");
-
-                long result = objCommon.GetDataExecuteScaler("SP_AddCRopeTaskStart", nv);
-
-
-                if (result > 0)
-                {
-                    Response.Redirect(String.Format("~/CropHealthReport.aspx?BatchLoc={0}&JobCode={1}}&CropAT={1}", BatchLoc, JobCode, result));
-                  //  Response.Redirect(String.Format("~/DumpTaskCompletion.aspx?Did={0}&Chid={1}&DrId={2}", result, ChId, Did));
-                }
             }
 
-        }
-
-
-        protected void btnSubmit_Click(object sender, EventArgs e)
-        {
-            long result = 0;
-            NameValueCollection nv = new NameValueCollection();
-            //  nv.Add("@SupervisorID", ddlSupervisor.SelectedValue);
-            //  nv.Add("@GrowerPutAwayId", lblGrowerID.Text);
-            // nv.Add("@WO",wo);
-
-            GridViewRow row = gvPlantReady.Rows[0];
-
-            var txtJobNo = (row.FindControl("lbljobID") as Label).Text;
-            var txtBenchLocation = (row.FindControl("lblBenchLoc") as Label).Text;
-
-            DataTable dt = new DataTable();
-            NameValueCollection nv1 = new NameValueCollection();
-            nv1.Add("@Aid", ddlCropAssignment.SelectedValue);
-            dt = objCommon.GetDataTable("spGeEmployeeRoleDetails", nv1);
-
-            nv.Add("@SupervisorID", ddlCropAssignment.SelectedValue);
-            nv.Add("@LoginID", Session["LoginID"].ToString());
-            nv.Add("@Did", HiddenFieldDid.Value);
-            nv.Add("@Comments", txtCommentsDump.Text);
-            nv.Add("@wo", "0");
-            nv.Add("@ManualID", HiddenFieldJid.Value);
-            nv.Add("@CropDate", txtCropDate.Text);
-            nv.Add("@QuantityOfTray", txtQuantityofTray.Text);
-            nv.Add("@RoleId", dt.Rows[0]["RoleID"].ToString());
-
-            nv.Add("@jobcode", txtJobNo);
-            nv.Add("@GreenHouseID", txtBenchLocation);
-
-
-
-            result = objCommon.GetDataInsertORUpdate("SP_AddCropReportManua", nv);
-
-
-            if (result > 0)
-            {
-                // ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Assignment Successful')", true);
-
-                string url = "";
-                if (Session["Role"].ToString() == "1")
-                {
-                    url = "MyTaskGrower.aspx";
-                }
-                else
-                {
-                    url = "MyTaskAssistantGrower.aspx";
-                }
-
-
-                string message = "Assignment Successful";
-                //string url = "MyTaskAssistantGrower.aspx";
-                string script = "window.onload = function(){ alert('";
-                script += message;
-                script += "');";
-                script += "window.location = '";
-                script += url;
-                script += "'; }";
-                ClientScript.RegisterStartupScript(this.GetType(), "Redirect", script, true);
-                // lblmsg.Text = "Assignment Successful";
-                clear();
-            }
-            else
-            {
-                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Assignment not Successful')", true);
-                //  lblmsg.Text = "Assignment Not Successful";
-            }
-        }
-
-        public void clear()
-        {
-
-            //  ddlSupervisor.SelectedIndex = 0;
+         
 
         }
 
-        protected void btnReset_Click(object sender, EventArgs e)
-        {
-            clear();
-            Response.Redirect("~/MyTaskAssistantGrower.aspx");
-        }
+
 
         protected void gvPlantReady_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
