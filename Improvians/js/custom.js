654b208c390b664a92d447761bd36383f96a1724
evo2 <evo2@MPH-GEM-02>
2021-04-19 02:49:01 -0700

    fixed swjs

--- Improvians/js/custom.js
+++ Improvians/js/custom.js
@@ -1,14 +1,23 @@
-jQuery(document).ready(function ($) {
-    /*
-    if ('serviceWorker' in navigator) {
-        console.log('CLIENT: service worker registration in progress.');
-        navigator.serviceWorker.register('./sw.js', { scope: './' }).then(function () {
-            console.log('CLIENT: service worker registration complete.');
-        }).catch(function (err) {
-            console.log("Service Worker Failed to Register", err);
-        })
-    } else {
-        console.log('CLIENT: service worker is not supported.');
-    }
-    */
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
