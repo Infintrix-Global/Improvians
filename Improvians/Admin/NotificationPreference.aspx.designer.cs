d7c3374cc7554602afb07963ae6da1645796cff5
evo3 <evo3@MPH-GEM>
2021-04-21 22:54:04 -0700

    notificaiton desgin changes

--- Improvians/Admin/NotificationPreference.aspx.designer.cs
+++ Improvians/Admin/NotificationPreference.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace Evo.Admin
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.ScriptManager SrciptManager1;
+
+        /// <summary>
+        /// gvUsers control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvUsers;
     }
 }
