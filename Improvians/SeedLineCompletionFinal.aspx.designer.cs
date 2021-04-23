b087032a906be960553fbb1286174831f467fc6b
evo1 <evo1@MPH-GEM>
2021-04-23 03:05:06 -0700

    Commit

--- Improvians/SeedLineCompletionFinal.aspx.designer.cs
+++ Improvians/SeedLineCompletionFinal.aspx.designer.cs
@@ -69,6 +69,24 @@ namespace Evo
         protected global::System.Web.UI.WebControls.Label lblJobID;
 
         /// <summary>
+        /// lblduedate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblduedate;
+
+        /// <summary>
+        /// lblSeedingDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSeedingDate;
+
+        /// <summary>
         /// radtraysize control.
         /// </summary>
         /// <remarks>
