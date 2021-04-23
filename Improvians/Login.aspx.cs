99349b394e089520251cbc459c01d38ca29993d5
evo1 <evo1@MPH-GEM>
2021-04-23 07:15:01 -0700

    Commit

--- Improvians/Login.aspx.cs
+++ Improvians/Login.aspx.cs
@@ -52,10 +52,22 @@ namespace Evo
                 {
                     Response.Redirect("~/Customer/CustomerDashBoard.aspx");
                 }
+               
                 else
                 {
                     Response.Redirect("~/DashBoard.aspx");
                 }
+
+
+
+                // else if (_dtLogin.Rows[0]["RoleID"].ToString() == "1") /*customer */
+                //{
+                //    Response.Redirect("~/CreateTask.aspx");
+                //}
+                //else if (_dtLogin.Rows[0]["RoleID"].ToString() == "12") /*customer */
+                //{
+                //    Response.Redirect("~/CreateTask.aspx");
+                //}
 
             }
         }
