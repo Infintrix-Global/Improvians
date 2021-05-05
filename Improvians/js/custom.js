0702c3d9c693134db28689e2914c4be03536755c
evo2 <evo2@MPH-GEM-02>
2021-05-04 23:33:43 -0700

    Dashboard chart

--- Improvians/js/custom.js
+++ Improvians/js/custom.js
@@ -9,4 +9,44 @@ jQuery(document).ready(function ($) {
     } else {
         console.log('CLIENT: service worker is not supported.');
     }
+
+    var todaysDate = new Date().toLocaleDateString('en-CA');
+    $(".todaysDate").val(todaysDate);
+
+    if ($("#task-distribution").length > 0) {
+        google.charts.load('current', {
+            callback: function () {
+                drawTaskDist();
+                $(window).resize(drawTaskDist);
+            },
+            packages: ['corechart', 'bar']
+        });
+
+        google.charts.setOnLoadCallback(drawTaskDist);
+        function drawTaskDist() {
+            var taskDistData = google.visualization.arrayToDataTable([
+                ['Profiles', 'Tasks'],
+                ['Assitant Grower', 50],
+                ['Grower', 16],
+                ['Sprayer', 5],
+                ['Irrigator', 0],
+                ['Crew Lead', 15],
+            ]);
+
+            var taskDistOptions = {
+                bar: { groupWidth: 30 },
+                legend: { position: 'none' },
+                title: 'Task Distribution of Profiles',
+            };
+
+            var taskDistChart = new google.visualization.ColumnChart(document.getElementById('task-distribution'));
+            taskDistChart.draw(taskDistData, taskDistOptions);
+
+            google.visualization.events.addListener(taskDistChart, 'ready', findSVG);
+
+            function findSVG() {
+                $("#task-distribution svg").attr("viewBox", "0 0 " + taskDistOptions.width + " " + taskDistOptions.height);
+            }
+        }
+    } //If ends
 });
