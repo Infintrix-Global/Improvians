2faff7359513b55122c62ce75db218a7925507d9
evo2 <evo2@MPH-GEM-02>
2021-05-05 06:24:23 -0700

    dashboard css js and page

--- Improvians/js/custom.js
+++ Improvians/js/custom.js
@@ -11,58 +11,135 @@ jQuery(document).ready(function ($) {
     }
 
    
-    var todaysDate = new Date().toLocaleDateString('en-CA');
-    $(".todaysDate").val(todaysDate);
 
-    if ($("#task-distribution").length > 0) {
+    var todaysDate = new Date().toLocaleDateString('en-CA'),
+        nextWeek = new Date(new Date(todaysDate).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString('en-CA');
+
+    function mmddyy(date) {
+        return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '-' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '-' + date.getFullYear();
+    }
+
+    $("#seeded_date_field, .todaysDate").val(todaysDate);
+    $(".todaysDateLabel").text(mmddyy(new Date(todaysDate)));
+
+    $(".nextWeekDate").val(nextWeek);
+    $(".nextWeekDateLabel").text(mmddyy(new Date(nextWeek)));
+
+
+    var taskDistOptions;
+
+    if ($(".googleChart").length > 0) {
+
+        taskDistOptions = {
+            chartArea: {
+                left: 150,
+                bottom: 70,
+            },
+            height: 300,
+            bar: { groupWidth: 30 },
+            legend: { position: 'none' },
+            colors: ['#438644'],
+            hAxis: { title: 'Task Distribution of Profiles', titleTextStyle: { fontStyle: 'bold' } },
+            vAxis: { title: 'Hours/Day', },
+            seriesType: 'bars',
+            series: { 1: { type: 'line', color: '#FF0000', lineWidth: 1, lineDashStyle: [10, 2] } },
+            fontSize: 14,
+        };
+
         google.charts.load('current', {
             callback: function () {
-                $.ajax(
-                    {
-                        type: 'POST',
-                        dataType: 'json',
-                        contentType: 'application/json',
-                        url: 'Dashboard.aspx/GetTaskDisctributionChartData',
-                        data: '{}',
-                        success: function (response) {
-                            drawTaskDist(response.d); // calling method  
-                        },
-
-                        error: function () {
-                            alert("Error loading data! Please try again.");
-                        }
-                    });
-                $(window).resize(drawTaskDist);
+                renderCharts();
+                $(window).resize(renderCharts);
             },
-            packages: ['corechart', 'bar']
+            packages: ['corechart']
         });
 
-        google.charts.setOnLoadCallback(drawTaskDist);
-        function drawTaskDist(dataValues) {
-            var taskDistData = new google.visualization.DataTable();
+        google.charts.setOnLoadCallback(renderCharts);
 
-            taskDistData.addColumn('string', 'EmployeeName');
-            taskDistData.addColumn('number', 'TaskHours');
-
-            for (var i = 0; i < dataValues.length; i++) {
-                taskDistData.addRow([dataValues[i].EmployeeName, dataValues[i].TaskHours]);
-            }
-            // Instantiate and draw our chart, passing in some options  
-                     
-            var taskDistOptions = {
-                bar: { groupWidth: 30 },
-                legend: { position: 'none' },
-                title: 'Task Distribution of Profiles',
-            };
-
-            var taskDistChart = new google.visualization.ColumnChart(document.getElementById('task-distribution'));
-            taskDistChart.draw(taskDistData, taskDistOptions);
-
-            google.visualization.events.addListener(taskDistChart, 'ready', findSVG);
-
-            function findSVG() {
-                $("#task-distribution svg").attr("viewBox", "0 0 " + taskDistOptions.width + " " + taskDistOptions.height);
-            }
+        function makeTitleBold() {
+            $(".googleChart").each(function () {
+                $(this).find("svg text").each(function () {
+                    if ($(this).attr("font-style") == 'italic') {
+                        $(this).attr("font-style", "").addClass("robotobold");
+                    }
+                })
+            });
         }
-    } //If ends
+
+        function renderCharts() {
+
+            //Dashboard Task Chart Call
+            if ($("#task-distribution").length > 0) { drawDashTaskDist(); }
+            if ($("#task-distribution-1").length > 0) { drawTaskDist1(); }
+            if ($("#task-distribution-2").length > 0) { drawTaskDist2(); }
+            if ($("#task-distribution-3").length > 0) { drawTaskDist3(); }
+
+            makeTitleBold();
+        }
+
+    }
+
+    //Dashboard Task Chart Function
+    function drawDashTaskDist() {
+        var taskDistData = google.visualization.arrayToDataTable([
+            ['Profiles', 'Tasks', 'Task Limit'],
+            ['Assitant Grower', 10, 8],
+            ['Supervisor', 12, 8],
+            ['Sprayer', 5, 8],
+            ['Irrigator', 0, 8],
+            ['Crew Lead', 15, 8],
+        ]);
+
+
+        taskDistOptions.height = 250;
+        taskDistOptions.chartArea.left = 100;
+
+        var taskDistChart = new google.visualization.ColumnChart(document.getElementById('task-distribution'));
+        taskDistChart.draw(taskDistData, taskDistOptions);
+    }
+
+    //Dashboard Task Chart Function
+    function drawTaskDist1() {
+        var taskDistData = google.visualization.arrayToDataTable([
+            ['Profiles', 'Tasks', 'Task Limit'],
+            ['Assitant Grower', 3, 8],
+            ['Supervisor', 8, 8],
+            ['Sprayer', 5, 8],
+            ['Irrigator', 13, 8],
+            ['Crew Lead', 6, 8],
+        ]);
+
+        var taskDistChart = new google.visualization.ColumnChart(document.getElementById('task-distribution-1'));
+        taskDistChart.draw(taskDistData, taskDistOptions);
+    }
+
+    //Dashboard Task Chart Function
+    function drawTaskDist2() {
+        var taskDistData = google.visualization.arrayToDataTable([
+            ['Profiles', 'Tasks', 'Task Limit'],
+            ['Assitant Grower', 2, 8],
+            ['Supervisor', 4, 8],
+            ['Sprayer', 6, 8],
+            ['Irrigator', 8, 8],
+            ['Crew Lead', 10, 8],
+        ]);
+
+        var taskDistChart = new google.visualization.ColumnChart(document.getElementById('task-distribution-2'));
+        taskDistChart.draw(taskDistData, taskDistOptions);
+    }
+
+    //Dashboard Task Chart Function
+    function drawTaskDist3() {
+        var taskDistData = google.visualization.arrayToDataTable([
+            ['Profiles', 'Tasks', 'Task Limit'],
+            ['Assitant Grower', 10, 8],
+            ['Supervisor', 12, 8],
+            ['Sprayer', 5, 8],
+            ['Irrigator', 3, 8],
+            ['Crew Lead', 15, 8],
+        ]);
+
+        var taskDistChart = new google.visualization.ColumnChart(document.getElementById('task-distribution-3'));
+        taskDistChart.draw(taskDistData, taskDistOptions);
+    }
 });
