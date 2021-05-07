2443caf71830ff01e77de5f6ef0c52c873bc6028
evo1 <evo1@MPH-GEM-02>
2021-05-07 04:57:15 -0700

    filters applied

--- Improvians/TaskDistributionChart.aspx.cs
+++ Improvians/TaskDistributionChart.aspx.cs
@@ -1,161 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace Evo
 {
     public partial class TaskDistributionChart : System.Web.UI.Page
     {
+        CommonControl objCommon = new CommonControl();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                txtFromDate.Text = Convert.ToDateTime(System.DateTime.Now).ToString("yyyy-MM-dd");
-                txtToDate.Text = Convert.ToDateTime(System.DateTime.Now.AddDays(7)).ToString("yyyy-MM-dd");
-
-                Session["Start"] = txtFromDate.Text;
-                Session["End"] = txtToDate.Text;
-                using (SqlConnection conn = new SqlConnection())
-                {
-                    conn.ConnectionString = ConfigurationManager.ConnectionStrings["Evo"].ConnectionString;
-                    using (SqlCommand cmd = new SqlCommand("SP_GetTaskDisctributionChartData", conn))
-                    {
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        SqlDataAdapter da = new SqlDataAdapter();
-                        List<ReportChartDetails> lstReportChartDetails = new List<ReportChartDetails>();
-                        int i = 0;
-
-                        DateTime StartDate = Convert.ToDateTime(HttpContext.Current.Session["Start"].ToString());
-                        DateTime EndDate = Convert.ToDateTime(HttpContext.Current.Session["End"].ToString());
-                        for (DateTime date = StartDate; date <= EndDate; date = date.AddDays(1))
-                        {
-                            ReportChartDetails reportdetails = new ReportChartDetails();
-                            i = i + 1;
-                            cmd.Parameters.Clear();
-                            cmd.Parameters.AddWithValue("@WorkDate", date);
-                            cmd.Parameters.AddWithValue("@LoginID", HttpContext.Current.Session["LoginID"].ToString());
-                            cmd.Parameters.AddWithValue("@Facility", HttpContext.Current.Session["Facility"].ToString());
-                            string strRoles = string.Empty;
-                            if (HttpContext.Current.Session["Role"].ToString() == "1")
-                            {
-                                strRoles = "1,2,3,5,11,12";
-                            }
-                            else if (HttpContext.Current.Session["Role"].ToString() == "12")
-                            {
-                                strRoles = "2,3,5,11,12";
-                            }
-                            else if (HttpContext.Current.Session["Role"].ToString() == "2")
-                            {
-                                strRoles = "3,5,11";
-                            }
-                            else
-                            {
-                                strRoles = HttpContext.Current.Session["Role"].ToString();
-                            }
-                            cmd.Parameters.AddWithValue("@RoleIDs", strRoles);
-                            da.SelectCommand = cmd;
-                            DataTable dt = new DataTable();
-                            da.Fill(dt);
-                            List<ChartDetails> lstChartDetails = new List<ChartDetails>();
-                            foreach (DataRow dtrow in dt.Rows)
-                            {
-                                ChartDetails details = new ChartDetails();
-                                details.EmployeeName = dtrow[0].ToString();
-                                details.TaskHours = Convert.ToInt32(dtrow[1]);
-
-                                lstChartDetails.Add(details);
-                            }
-                            reportdetails.ID = i;
-                            reportdetails.WorkDate = date;
-                            reportdetails.lstChartDetail = lstChartDetails;
-                            lstReportChartDetails.Add(reportdetails);
-                        }
-                        conn.Close();
-                        repChart.DataSource = lstReportChartDetails;
-                        repChart.DataBind();
-                    }
-                }
-
+                txtFromDate.Text = Convert.ToDateTime(System.DateTime.Now.AddDays(-1)).ToString("yyyy-MM-dd");
+                txtToDate.Text = Convert.ToDateTime(System.DateTime.Now.AddDays(4)).ToString("yyyy-MM-dd");
+                BindRoles();
+                BindData();
             }
+        }
+        private void BindRoles()
+        {
+            NameValueCollection nv = new NameValueCollection();
+            nv.Add("@RoleID", Session["Role"].ToString());
+            DataTable dt = objCommon.GetDataTable("SP_GetRoleForAssignement", nv);
+            repRoles.DataSource = dt;
+            repRoles.DataBind();
+        }
+        private void BindData()
+        {
+            List<ReportChartDetails> lstReportChartDetails = new List<ReportChartDetails>();
+            int i = 0;
+
+            DateTime StartDate = Convert.ToDateTime(txtFromDate.Text);
+            DateTime EndDate = Convert.ToDateTime(txtToDate.Text);
+            for (DateTime date = StartDate; date <= EndDate; date = date.AddDays(1))
+            {
+                ReportChartDetails reportdetails = new ReportChartDetails();
+                i = i + 1;
+                reportdetails.ID = i;
+                reportdetails.DayofWorkDate = date.DayOfWeek.ToString();
+                reportdetails.WorkDate = date;
+                lstReportChartDetails.Add(reportdetails);
+            }
+            repChart.DataSource = lstReportChartDetails;
+            repChart.DataBind();
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-
+           
+            BindData();
         }
 
         protected void repChart_ItemDataBound(object sender, RepeaterItemEventArgs e)
         {
-
+            ReportChartDetails dr = (ReportChartDetails)e.Item.DataItem;
+            Literal ltScript = (Literal)e.Item.FindControl("ltScripts");
+            HtmlControl chartDivid = (HtmlControl)e.Item.FindControl("divTask");
+            BindChart(dr.WorkDate, chartDivid.ClientID, ltScript);
         }
-
-        [System.Web.Script.Services.ScriptMethod()]
-        [System.Web.Services.WebMethod]
-        public static List<ReportChartDetails> GetTaskDisctributionChartData()
+        public DataTable GetChartData(string date)
         {
-            using (SqlConnection conn = new SqlConnection())
+            NameValueCollection nv = new NameValueCollection();
+            nv.Add("@WorkDate", date);
+            string strEmployeeIDs = string.Empty;
+            foreach (RepeaterItem item in repRoles.Items)
             {
-                conn.ConnectionString = ConfigurationManager.ConnectionStrings["Evo"].ConnectionString;
-                using (SqlCommand cmd = new SqlCommand("SP_GetTaskDisctributionChartData", conn))
+                CheckBox chkRole = (CheckBox)item.FindControl("chkRole");             
+                if (chkRole.Checked)
                 {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    SqlDataAdapter da = new SqlDataAdapter();
-                    List<ReportChartDetails> lstReportChartDetails = new List<ReportChartDetails>();
-                    int i = 0;
-
-                    DateTime StartDate = Convert.ToDateTime(HttpContext.Current.Session["Start"].ToString());
-                    DateTime EndDate = Convert.ToDateTime(HttpContext.Current.Session["End"].ToString());
-                    for (DateTime date = StartDate; date <= EndDate; date = date.AddDays(1))
-                    {
-                        ReportChartDetails reportdetails = new ReportChartDetails();
-                        i = i + 1;
-                        cmd.Parameters.AddWithValue("@WorkDate", date);
-                        cmd.Parameters.AddWithValue("@LoginID", HttpContext.Current.Session["LoginID"].ToString());
-                        cmd.Parameters.AddWithValue("@Facility", HttpContext.Current.Session["Facility"].ToString());
-                        string strRoles = string.Empty;
-                        if (HttpContext.Current.Session["Role"].ToString() == "1")
-                        {
-                            strRoles = "1,2,3,5,11,12";
-                        }
-                        else if (HttpContext.Current.Session["Role"].ToString() == "12")
-                        {
-                            strRoles = "2,3,5,11,12";
-                        }
-                        else if (HttpContext.Current.Session["Role"].ToString() == "2")
-                        {
-                            strRoles = "3,5,11";
-                        }
-                        else
-                        {
-                            strRoles = HttpContext.Current.Session["Role"].ToString();
-                        }
-                        cmd.Parameters.AddWithValue("@RoleIDs", strRoles);
-                        da.SelectCommand = cmd;
-                        DataTable dt = new DataTable();
-                        da.Fill(dt);
-                        List<ChartDetails> lstChartDetails = new List<ChartDetails>();
-                        foreach (DataRow dtrow in dt.Rows)
-                        {
-                            ChartDetails details = new ChartDetails();
-                            details.EmployeeName = dtrow[0].ToString();
-                            details.TaskHours = Convert.ToInt32(dtrow[1]);
-
-                            lstChartDetails.Add(details);
-                        }
-                        reportdetails.ID = i;
-                        reportdetails.WorkDate = date;
-                        reportdetails.lstChartDetail = lstChartDetails;
-                        lstReportChartDetails.Add(reportdetails);
-                    }
-                    conn.Close();
-                    return lstReportChartDetails;
+                    strEmployeeIDs += ((HiddenField)item.FindControl("hdnValue")).Value + ",";
                 }
+            }
+            nv.Add("@RoleIDs", strEmployeeIDs);
+            nv.Add("@LoginID", Session["LoginID"].ToString());
+            return objCommon.GetDataTable("SP_GetTaskDisctributionChartFilterData", nv);
+        }
+        private void BindChart(DateTime date, string chartDivid, Literal ltScript)
+        {
+            DataTable dsChartData = new DataTable();
+            StringBuilder strScript = new StringBuilder();
+
+            try
+            {
+                dsChartData = GetChartData(date.ToString("MM-dd-yyyy"));
+
+                strScript.Append(@" <script type='text/javascript'>  
+                    google.load('visualization', '1', {packages: ['corechart']}); </script>  
+                      
+                    <script type='text/javascript'>  
+                     
+                    function drawChart() {         
+                    var data = google.visualization.arrayToDataTable([  
+                    ['Task Distribution of Profiles', 'Hours / Day'],");
+
+                foreach (DataRow row in dsChartData.Rows)
+                {
+                    strScript.Append("['" + row[0].ToString() + "'," + row[1].ToString() + "],");
+                }
+
+                strScript.Remove(strScript.Length - 1, 1);
+                strScript.Append("]);");
+
+                strScript.Append(@" var options = {     
+                                    chartArea: {
+                                                    left: 150,
+                                                    bottom: 70,
+                                                },
+                                                height: 300,
+                                                bar: { groupWidth: 30 },
+                                                legend: { position: 'none' },
+                                                colors: ['#438644'],
+                                                hAxis: { title: 'Task Distribution of Profiles', titleTextStyle: { fontStyle: 'bold' } },
+                                                vAxis: { title: 'Hours/Day', },
+                                                seriesType: 'bars',
+                                                series: { 1: { type: 'line', color: '#FF0000', lineWidth: 1, lineDashStyle: [10, 2] } },
+                                                fontSize: 14,          
+                                    };   ");
+
+                strScript.Append(@"var chart = new google.visualization.ColumnChart(document.getElementById('");
+                strScript.Append(chartDivid);
+                strScript.Append(@"'));           
+                chart.draw(data, options);
+            }
+                            google.setOnLoadCallback(drawChart);
+            ");
+                strScript.Append(" </script>");
+
+                ltScript.Text = strScript.ToString();
+            }
+            catch
+            {
+            }
+            finally
+            {
+                dsChartData.Dispose();
+                strScript.Clear();
             }
         }
 
--- Improvians/TaskDistributionChart.aspx.designer.cs
+++ Improvians/TaskDistributionChart.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace Evo
         protected global::System.Web.UI.WebControls.TextBox txtToDate;
 
         /// <summary>
+        /// repRoles control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repRoles;
+
+        /// <summary>
         /// btnSubmit control.
         /// </summary>
         /// <remarks>
