327e54fa5e91cb48a0c68b615b39578fa311fee2
evo1 <evo1@MPH-GEM-02>
2021-05-10 04:56:01 -0700

    validations

--- Improvians/ForgetPassword.aspx.cs
+++ Improvians/ForgetPassword.aspx.cs
@@ -33,23 +33,28 @@ namespace Evo
             if (dt != null && dt.Rows.Count > 0)
             {
                 string ReceiverEmail = dt.Rows[0]["Email"].ToString();
-                string ReceiverPassword = dt.Rows[0]["Password"].ToString();
-                string Subject = "Forget Password";
-                string msg = "Hi " + txtUserName.Text + "," + "<br /><br />";
-                msg = msg + "Your password to access portal is: " + objCom.Decrypt(ReceiverPassword) + "<br />";
+                if (string.IsNullOrEmpty(ReceiverEmail))
+                    lblmsg.Text = "Email ID not found";
+                else
+                {
+                    string ReceiverPassword = dt.Rows[0]["Password"].ToString();
+                    string Subject = "Forget Password";
+                    string msg = "Hi " + txtUserName.Text + "," + "<br /><br />";
+                    msg = msg + "Your password to access portal is: " + objCom.Decrypt(ReceiverPassword) + "<br />";
 
-                msg = msg + "<br />Thanks, <br/> Growers Transplanting, Inc Portal";
-                objGen.SendMail(ReceiverEmail, "", Subject, msg);
+                    msg = msg + "<br />Thanks, <br/> Growers Transplanting, Inc Portal";
+                    objGen.SendMail(ReceiverEmail, "", Subject, msg);
 
-                string message = "Thank you for your request. Password has been sent to you over an email.";
-                string url = "Login.aspx";
-                string script = "window.onload = function(){ alert('";
-                script += message;
-                script += "');";
-                script += "window.location = '";
-                script += url;
-                script += "'; }";
-                ClientScript.RegisterStartupScript(this.GetType(), "Redirect", script, true);
+                    string message = "Thank you for your request. Password has been sent to you over registered email id.";
+                    string url = "Login.aspx";
+                    string script = "window.onload = function(){ alert('";
+                    script += message;
+                    script += "');";
+                    script += "window.location = '";
+                    script += url;
+                    script += "'; }";
+                    ClientScript.RegisterStartupScript(this.GetType(), "Redirect", script, true);
+                }
             }
             else
             {
