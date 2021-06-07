f5eded5b61c07b29fd1379e5db8263732a36cc9e
evo1 <evo1@MPH-GEM>
2021-06-07 06:08:34 -0700

    Commit

--- Improvians/MyTaskGrower.aspx.cs
+++ Improvians/MyTaskGrower.aspx.cs
@@ -475,7 +475,7 @@ namespace Evo
             nv.Add("@JobCode", "0");
             nv.Add("@CustomerName", "0");
             nv.Add("@Facility", Session["Facility"].ToString());
-            nv.Add("@BenchLocation", "0");
+            nv.Add("@BenchLocation", "");
             nv.Add("@Crop", "0");
             nv.Add("@Status", "");
             nv.Add("@Jobsource", "0");
@@ -501,7 +501,7 @@ namespace Evo
             nv.Add("@CustomerName", "0");
             nv.Add("@Facility", Session["Facility"].ToString());
             nv.Add("@Status", "0");
-            nv.Add("@BenchLocation", "0");
+            nv.Add("@BenchLocation", "");
             nv.Add("@Jobsource", "");
             nv.Add("@LoginId", Session["LoginID"].ToString());
             nv.Add("@FromDate", "");
