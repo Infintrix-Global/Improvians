74a7f94d6cb85620cbe28c3c9eafd50223553589
evo1 <evo1@MPH-GEM>
2021-06-24 10:36:20 -0700

    Commit

--- Improvians/BAL_Classes/BAL_Task.cs
+++ Improvians/BAL_Classes/BAL_Task.cs
@@ -310,8 +310,8 @@ namespace Evo.BAL_Classes
             DataTable dt = new DataTable();
             try
             {
-                strQuery = "Select GTS.jobcode,GTS.wo,GPD.GrowerPutAwayId,cname,GTS.itemdescp,GTS.itemno,GPD.FacilityID,GPD.GreenHouseID, GPD.Trays,GTS.TraySize,STC.SeededDate as SeededDate,GTS.GenusCode,'0' as jid " +
-                            "  from gti_jobs_seeds_plan GTS inner join SeedLineTaskCompletion STC on STC.wo=GTS.wo inner join GrowerPutAwayDetails GPD on GPD.wo=GTS.wo " +
+                strQuery = "Select GTS.jobcode,GTS.wo,GPD.GrowerPutAwayId,cname,GTS.itemdescp,GTS.itemno,GPD.FacilityID,GPD.GreenHouseID,MC.TraysMoved as Trays,GTS.TraySize,STC.SeededDate as SeededDate,GTS.GenusCode,'0' as jid " +
+                            "  from gti_jobs_seeds_plan GTS inner join SeedLineTaskCompletion STC on STC.wo=GTS.wo inner join GrowerPutAwayDetails GPD on GPD.wo=GTS.wo inner join MoveCompletionDetails MC on MC.GrowerPutAwayId=GPD.GrowerPutAwayId " +
 
                             "where GPD.IsActive=0 ";
                 if (!string.IsNullOrEmpty(FacilityLocation))
