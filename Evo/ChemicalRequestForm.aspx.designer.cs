cbffcee8ac903e28fa6b88862d34b8e23fd2fc59
evo1 <evo1@MPH-GEM-02>
2021-07-12 23:49:19 -0700

    fix data__mobile

--- Evo/ChemicalRequestForm.aspx.designer.cs
+++ Evo/ChemicalRequestForm.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace Evo
         protected global::AjaxControlToolkit.AutoCompleteExtender AutoCompleteExtender1;
 
         /// <summary>
+        /// ddlBenchLocation control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ddlBenchLocation;
+
+        /// <summary>
         /// ddlJobNo control.
         /// </summary>
         /// <remarks>
